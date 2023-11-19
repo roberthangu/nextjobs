7cca4490330c108e7992288a9f2c0c5d6375eae8
Robert Hangu <robert.hangu@gmail.com>
2023-11-19 22:29:48 +0100

    Remove footer bar and add simple copyright

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
 import Container from "react-bootstrap/Container";
 import JobSearch from "./JobSearch";
 import Navbar from "react-bootstrap/Navbar";
+import Stack from "react-bootstrap/esm/Stack";
 
 function App() {
     return (
         <>
-            <Navbar style={{ background: "#e7e7e7" }} fixed="top" className="shadow">
+            <Navbar
+                style={{ background: "#e7e7e7" }}
+                fixed="top"
+                className="shadow"
+            >
                 <Container>
                     <Navbar.Brand>Next Jobs</Navbar.Brand>
                     <Navbar.Text>AI-Powered Job Board</Navbar.Text>
                 </Container>
             </Navbar>
-            <Container style={{ marginTop: "100px" }}>
-                <JobSearch />
-            </Container>
-            <Navbar
-                style={{ background: "#e7e7e7", height: "40px" }}
-                fixed="bottom"
+            <JobSearch />
+            <Stack
+                direction="horizontal"
+                className="justify-content-center text-secondary"
             >
-                <Container>
-                    <Navbar.Text>
-                        (c) 2023{" "}
-                        <a href="https://nextops.agency/?utm_source=nextjobs&utm_medium=footer">
-                            Next Operations
-                        </a>
-                    </Navbar.Text>
-                </Container>
-            </Navbar>
+                <span>
+                    (c) 2023{" "}
+                    <a href="https://nextops.agency/?utm_source=nextjobs&utm_medium=footer">
+                        Next Operations
+                    </a>
+                </span>
+            </Stack>
         </>
     );
 }
